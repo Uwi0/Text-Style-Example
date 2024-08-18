4dfe55b175480ffcb2aca0f39e6c588352196365
uwi0 <kakapoweb@gmail.com>
2024-08-19 04:22:06 +0700

    Initial Commit

---
+++ Text Style Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Text Style Example
+//
+//  Created by dwi prasetyo on 19/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Text Style Example/Text_Style_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_Style_ExampleApp.swift
+//  Text Style Example
+//
+//  Created by dwi prasetyo on 19/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Text_Style_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
